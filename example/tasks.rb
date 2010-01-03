3bf02bc7957bf40f5221bc8e3eea9392367a250a
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-03 17:19:54 +0100

    More documentation for hooks

--- example/tasks.rb
+++ example/tasks.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env nake
 
+# TODO: rewrite the example to Rango, it would be more interesting
+# Or maybe add more examples for jekyll, nanoc, Django, maybe Rails.
 $:.unshift(File.join(File.dirname(__FILE__), "..", "tasks"))
 
 load "git-deployer.nake"
 
-Task["deployer:setup"].config[:user] = "root"
-Task["deployer:setup"].config[:host] = "tagadab"
+Task["deployer:setup"].config[:user] = "TODO"
+Task["deployer:setup"].config[:host] = "TODO"
 Task["deployer:setup"].config[:path] = "/var/www/deploy-test"
 
